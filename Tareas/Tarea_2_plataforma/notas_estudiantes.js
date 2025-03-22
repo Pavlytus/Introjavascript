1efffd0b378df889e5e1bc302a13c81457756a9f
Pavlytus <pavlo.6cn@gmail.com>
2025-03-22 14:18:00 -0600

    tarea dos de la plataforma

---
+++ Tareas/Tarea_2_plataforma/notas_estudiantes.js
@@ -0,0 +1,15 @@
+// Este código permite asignar un mensaje personalizado, según las notas obtenidas por los estudiantes
+
+let nota=50
+if(nota>=90){
+console.log("Excelente")
+}
+else if(nota>=75&&nota<=89){
+console.log("Bien")
+}
+else if(nota>=60&&nota<=74){
+console.log("Sufiente")
+}
+else{
+console.log("No aprueba")
+}
